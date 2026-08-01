653e956f46950fb67ecd3d69827f0d4f4f9b5b77
GWrudra <rudrapnayak111@gmail.com>
2026-08-01 14:12:50 +0530

    Structure improved

--- main.py
+++ main.py
@@ -1,73 +1,97 @@
 import pandas as pd
 
-#File path as input from user
-csv_file_path = input("Enter the path of the CSV file :")
+def show_size(csv):
+    row,col=csv.shape
+    print("\nNo of Rows and Cols are :")
+    print(f"Rows --> {row}")
+    print(f"Columns --> {col}")
 
-#Reading csv file from path
-csv_file=pd.read_csv(csv_file_path)
+def show_datatype(csv):
+    print("\nColumns --> DataType")
+    for cur in csv.columns:
+        print(f"{cur} --> {csv[cur].dtype}")
 
-#Size of the CSV file
-row,col=csv_file.shape
-print(f"\nRows: {row}")
-print(f"Columns: {col}")
+def show_missing_values(csv):
+    print("\nNo of null Values in each column are :\n",csv.isnull().sum())
 
-#Shows Columns
-print(f"\nColumns={csv_file.columns}")
+def show_statistics(csv):
+    #If Sales present 
+    sf=True
+    for col in csv.columns:
+        if col.lower()=='sales':
+            print(f"\nHighest Sales {csv['Sales'].max()}")
+            print(f"Lowest Sales {csv['Sales'].min()}")
+            print(f"Sales Sum {csv['Sales'].sum()}")
+            print(f"Sales Mean {csv['Sales'].mean()}")
+            print(f"Sales Median {csv['Sales'].median()}\n")
+            sf=False
 
-#============================================================
+    #Else for any other Columns
+    if sf:
+        ch=input("\nDo you want to perform numberical operation on any col ? (Yes or No): ")
+        if ch.lower()=="yes":
+            ucol=input("Enter a valid column with numerical's")
+            if ucol not in csv.columns :
+                print("Sorry its invalid\n")
+            elif not pd.api.types.is_numeric_dtype(csv[ucol]):
+                print("Sorry its not numeric\n")
+            else:
+                print(f"Lowest {ucol} {csv[ucol].min()}")
+                print(f"Highest {ucol} {csv[ucol].max()}")
+                print(f"{ucol} Sum {csv[ucol].sum()}")
+                print(f"{ucol} Mean {csv[ucol].mean()}")
+                print(f"{ucol} Median {csv[ucol].median()}\n")                
 
-#Finds Missing values
-print("\nNo of null Values in each column are :\n",csv_file.isnull().sum())
-
-#============================================================
-
-#Calculating The max,min,sum,mean,medain
-
-#For Sales if present 
-sf=True
-for col in csv_file.columns:
-    if col=="Sales":
-        print(f"\nHighest Sales {csv_file["Sales"].max()}")
-        print(f"Lowest Sales {csv_file["Sales"].min()}")
-        print(f"Sales Sum {csv_file["Sales"].sum()}")
-        print(f"Sales Mean {csv_file["Sales"].mean()}")
-        print(f"Sales Median {csv_file["Sales"].median()}\n")
-        sf=False
-
-#Else for any other Columns
-if sf:
-    ch=input("Do you want to perform numberical operation on any col ? (Yes or No): ")
+def data_cleaning(csv):
+    ch=input("Do you want to replace the missing values ? --> Yes or No :")
     if ch.lower()=="yes":
-        ucol=input("Enter a valid column with numerical's")
-        if ucol in csv_file:
-            print(f"Lowest {ucol} {csv_file[ucol].min()}")
-            print(f"Highest {ucol} {csv_file[ucol].max()}")
-            print(f"{ucol} Sum {csv_file[ucol].sum()}")
-            print(f"{ucol} Mean {csv_file[ucol].mean()}")
-            print(f"{ucol} Median {csv_file[ucol].median()}\n")
-        else:
-            print("Sorry its invalid\n")
+        for col in csv.columns:
+            if pd.api.types.is_numeric_dtype(csv[col]):
+                #Replacing NAN values with meadian
+                csv[col]=csv[col].fillna(csv[col].median())
+            else:
+                #Replacing NAN text with Unknown
+                csv[col] = csv[col].fillna("Unknown")
 
-#============================================================
+def save_file(csv):
+    ch=input("Do you want to save the cleaned data (Yes or No) :")
+    if ch.lower()=="yes":
+        fname=input("Enter file name(With .csv at the end) :")
+        csv.to_csv(fname,index=False)
+        print("\nCSV is Saved as cleaned_data")
 
-#Data Cleaning
-ch=input("Do you want to replace the missing values ? --> Yes or No :")
-if ch.lower()=="yes":
-    for col in csv_file.columns:
-        if pd.api.types.is_numeric_dtype(csv_file[col]):
-            #Replacing NAN values with meadian
-            csv_file[col]=csv_file[col].fillna(csv_file[col].median())
-        else:
-            #Replacing NAN text with Unknown
-            csv_file[col] = csv_file[col].fillna("Unknown")
+def main():
+    #File path as input from user
+    csv_file_path = input("Enter the path of the CSV file :")
 
-#============================================================
+    #Reading csv file from path
+    try:
+        csv_file=pd.read_csv(csv_file_path)
+    except FileNotFoundError:
+        print("file not found.")
+    except Exception as e:
+        print(e)
+        return
 
-#Saving the Creared file
-csv_file.to_csv("cleaned_data.csv",index=False)
-print("\nCSV is Saved as cleaned_data")
+    #Size of the CSV file
+    show_size(csv_file)
 
-#============================================================
+    #Shows Columns and Datatype
+    show_datatype(csv_file)
 
-# print(csv_file.describe().head())
-# print(csv_file.head())
+    #Finds Missing values
+    show_missing_values(csv_file)
+
+    #Calculating The max,min,sum,mean,medain
+    show_statistics(csv_file)
+
+    #Data Cleaning
+    data_cleaning(csv_file)
+
+    #Saving the Creared file
+    save_file(csv_file)
+
+    # print(csv_file.describe().head())
+    # print(csv_file.head())
+
+main()
